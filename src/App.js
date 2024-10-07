ec89b63eb6bfc8c5ff9ebb9372afcf4f4a9fb35f
Jugurtha <saadajugurtha59@gmail.com>
2024-10-07 10:48:36 +0200

    Add component num

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
 import Numero from './Numero';
 
 import logo from './logo.svg';
 import './App.css';
-=======
-import Alphabets from "./Alphabets";
-
-import logo from "./logo.svg";
-import "./App.css";
->>>>>>> ac2f8fd1b64c455c5c4d5e7f99f6463831adbd5b
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
         <Numero />
-=======
-        <Alphabets />
->>>>>>> ac2f8fd1b64c455c5c4d5e7f99f6463831adbd5b
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
